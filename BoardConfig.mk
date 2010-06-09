901352a10d5c4498d6c9d6cafa674c693b09a553
Erik Gilling <konkers@android.com>
2010-06-09 00:28:02 -0700

    remove command line from boot image so we can tell the diff between board revs
    Change-Id: Idd22716965f872976b095e0a3231307b6e860428

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,4 @@ BOARD_USES_GENERIC_AUDIO := true
 USE_CAMERA_STUB := true
 USE_E2FSPROGS := true
 
-BOARD_KERNEL_CMDLINE := mem=448M@0M console=ttyS0,115200n8 tegrapart=mmcblk0=system:3600:10000:800,cache:13600:4000:800,userdata:17600:80000:800 debug
-
 
