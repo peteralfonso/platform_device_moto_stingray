eee8cb3659219ce8c2ff95d60dc60e42e2c609f8
Ying Wang <wangying@google.com>
2010-09-21 16:58:35 -0700

    Make Stingray inherit from generic_no_telephony.mk
    Bug: 2994028
    Change-Id: I33b341f2bc41a11842cd8ae7aba0f15ecc6ace60

--- generic_stingray.mk
+++ generic_stingray.mk
@@ -1,7 +1,7 @@
 # Add CameraBrowser for testing PTP support until Gallery supports it
 PRODUCT_PACKAGES := CameraBrowser
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
 $(call inherit-product, device/moto/stingray/device.mk)
 $(call inherit-product-if-exists, vendor/moto/stingray/stingray-vendor.mk)
 
