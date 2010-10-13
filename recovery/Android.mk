f0e53d65fef15237416496fcdd32d4a4416214c1
Kazuhiro Ondo <kazuhiro.ondo@motorola.com>
2010-10-13 18:31:03 -0500

    Support of BP master clear in recovery.
    Commit for Cloud Li (a17470@motorola.com)
    
    Change-Id: I56f6126304f23bdcff82d49af62c26fef5bdbbaf

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := eng
 
-LOCAL_SRC_FILES := recovery_ui.c
+LOCAL_SRC_FILES := recovery_ui.c masterclear_bp.c
 LOCAL_C_INCLUDES += bootable/recovery
 # should match TARGET_RECOVERY_UI_LIB set in BoardConfig.mk
 LOCAL_MODULE := librecovery_ui_stingray
