247cc3ddbb0598f39d45424168adb90792dacc03
Hung-ying Tyan <tyanh@google.com>
2011-01-06 13:14:35 +0800

    Enable VOIP API.
    Bug: 3251016
    Change-Id: Idb4aae7ec067639283fe07cf0a2b0cfb75075b22

--- device.mk
+++ device.mk
@@ -40,7 +40,7 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-		frameworks/base/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+		frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 PRODUCT_COPY_FILES += \
