35b03f9cefb311bcf6adb4e88c005aa2a04eaad3
John Michelau <john.michelau@motorola.com>
2011-06-18 18:22:12 -0500

    Use netcat idle timeout to prevent bugreport from hanging
    If the LTE BP panics during a bugreport, netcat can hang forever and
    prevent dumpstate from finishing, unless a timeout is specified.
    
    Change-Id: I0b8341c30e4f115fcb78ba0270379ab07b3fc0d9

--- ril/wrigley-dump.sh
+++ ril/wrigley-dump.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-echo "Dumping LCM logs"
+echo "Dumping Wrigley logs"
 echo "I am running as $(id)"
 hostName=$(getprop ro.product.device)
 
@@ -17,7 +17,8 @@ echo "file:begin:txt:/${hostName}-ipv6_route.txt"
 cat /proc/net/ipv6_route
 echo "file:end:txt:/${hostName}-ipv6_route.txt"
 
-# Get the rest of the info from dumpd
+# Get the rest of the info from dumpd.  Use a 15-second keepalive timer to
+# ensure we don't hang on BP panics.
 for cmd in "state" "logs" "files" "panic" "atvc"; do
-    echo "-o wrigley $cmd" | nc 192.168.20.2 3002
+    echo "-o wrigley $cmd" | nc -w 15 192.168.20.2 3002
 done
